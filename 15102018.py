b7ee9bbcc0bf12975e3010936a47e81713495eeb
Denton72 <43029623+Denton72@users.noreply.github.com>
2018-10-15 15:59:39 +0300

    Add files via upload

---
+++ 15102018.py
@@ -0,0 +1,22 @@
+#IF ELSE
+'''
+x = input("Number:")
+x = int(x)
+print(x)
+if x > 2 :
+    print('Bigger')
+else :
+    print('Smaller')
+print('All done')
+'''
+#Multi-way IF elif else
+x = input("Number:")
+x = int(x)
+print(x)
+if x < 2 :
+    print('small')
+elif x < 10 :
+    print('Medium')
+else:
+    print('LARGE')
+print('All done')
